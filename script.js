110e6fb05d5754f03e2ebeaa2f3f3dad18d0b7eb
Shriprasanna B <shriprasanna32@gmail.com>
2022-04-29 00:24:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const check = document.querySelector(".check");
+const bg = document.querySelector(".bg");
+const para= document.querySelector(".default");
+const themeTxt= document.querySelector(".mode");
+const iconChange= document.querySelector(".icon");
+const footer= document.querySelector(".footer");
+
+check.addEventListener("change", (e) => {
+    bg.classList.toggle("change");
+    para.classList.toggle("pchange");
+    themeTxt.classList.toggle("pchange");  
+    footer.classList.toggle("fchange");  
+    iconChange.innerHTML=`<i style="color:white" class="fa-solid fa-moon"></i>`;
+});
