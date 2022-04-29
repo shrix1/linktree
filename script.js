18c143e67c5276a5f342f34be9ea91eaeda1c4bd
Shriprasanna B <shriprasanna32@gmail.com>
2022-04-29 18:43:01 +0530

    main

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const para = document.querySelector(".default");
 const themeTxt = document.querySelector(".mode");
 const iconChange = document.querySelector(".icon");
 const footer = document.querySelector(".footer");
+const gametxt= document.querySelector(".gametxt");
 
 check.addEventListener("change", (e) => {
     bg.classList.toggle("change");
     para.classList.toggle("pchange");
     themeTxt.classList.toggle("pchange");  
     footer.classList.toggle("fchange");  
+    gametxt.classList.toggle("gchange");  
     iconChange.innerHTML=`<i style="color:white" class="fa-solid fa-sun"></i>`;
 });
 
