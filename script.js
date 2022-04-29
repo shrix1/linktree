2a21ef5ef2a45d580fde0d0476520f119332b9a2
Shriprasanna B <shriprasanna32@gmail.com>
2022-04-29 22:13:50 +0530

    main'

--- script.js
+++ script.js
@@ -14,8 +14,3 @@ check.addEventListener("change", (e) => {
     gametxt.classList.toggle("gchange");  
     iconChange.innerHTML=`<i style="color:white" class="fa-solid fa-sun"></i>`;
 });
-
-
-
-// chick img https://cdn-icons-png.flaticon.com/512/141/141737.png
-// net img https://cdn-icons.flaticon.com/png/512/924/premium/924189.png?token=exp=1651238713~hmac=8fe040677082d3187deb90f257fd009e
